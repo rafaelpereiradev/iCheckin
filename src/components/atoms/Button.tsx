345609c100715eedc4d4d39ae56e58a433d6ed5d
Rafael de Souza Pereira <57875932+rafaelpereiradev@users.noreply.github.com>
2022-12-12 18:51:49 +0000

    Configurado codespaces para dev

--- src/components/atoms/Button.tsx
+++
@@ -1,7 +0,0 @@
-import ButtonMui,{ButtonProps} from "@mui/material/Button";
-
-export default function Button(props:ButtonProps) {
-  return (
-    <ButtonMui {...props} />
-  )
-}
--- src/components/atoms/LoadingButton.tsx
+++
@@ -1,7 +0,0 @@
-import LoadingButtonMui,{LoadingButtonProps} from "@mui/lab/LoadingButton";
-
-export default function LoadingButton(props:LoadingButtonProps) {
-  return (
-    <LoadingButtonMui {...props } />
-  )
-}
